866d50f5eb37ed483000ed8c82e19c90a0be4d35
138497 <subramani.murthy@cognizant.com>
2020-10-27 04:19:55 -0700

    updated

--- src/main/java/com/cognizant/collector/artifactory/ArtifactoryApplication.java
+++ src/main/java/com/cognizant/collector/artifactory/ArtifactoryApplication.java
@@ -6,11 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableConfigurationProperties
 @EnableScheduling
+@EnableAutoConfiguration
+@EnableEncryptableProperties
 public class ArtifactoryApplication {
 
 	public static void main(String[] args) {
